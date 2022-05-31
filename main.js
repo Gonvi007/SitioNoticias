6c1904fca79695f50e60ee41b2974c8d8ae9d6de
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-05-31 18:33:04 -0500

    se agrego el boton de desplazamiento con JS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Para desplegar el menu al ser un tamaño menor de 700px 
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
-});
+}); 
